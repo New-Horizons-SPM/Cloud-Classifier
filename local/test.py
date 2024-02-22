20b00e102e8e6a502f627dcd22e55b610c1e626c
ceds92 <julian.ceddia@monash.edu>
2024-02-23 09:20:31 +1100

    Add comments

--- local/test.py
+++ local/test.py
@@ -1,18 +1,18 @@
 import requests
 from ssh import ssh
 
-tunnel = ssh(user='ubuntu',host='118.xxx.xxx.xx')
+tunnel = ssh(user='ubuntu',host='118.138.241.17')   # Tunnel must be created before we can reach the nectar machine
 if(not tunnel.connected):
     exit()
 
-url = "http://127.0.0.1:8000/predict"
+url = "http://127.0.0.1:8000/predict"               # Predict endpoint. 8000 comes from the default port in ssh
 
-payload = {'scanData':  [1, 2, 3],
+payload = {'scanData':  [1, 2, 3],                  # Some dummy data
            'scanSize':  (75e-9,75e-9),
            'modelName': "test_model"}
 
-json = requests.post(url,json=payload)
-response = json.json()
+json = requests.post(url,json=payload)              # Make the post request
+response = json.json()                              # Convert json to Python dictionary
 
 prediction   = response['prediction']
 errorMessage = response['error']
